8348c75331f1f74d14c4aaa07c12572a5ff6ed42
knarjp <kevajess@gmail.com>
2016-11-01 18:33:39 -0400

    non trend employeeSelector

--- src/com/store/data/generator/utils/EmployeeSelector.java
+++ src/com/store/data/generator/utils/EmployeeSelector.java
@@ -2,26 +2,33 @@ package com.store.data.generator.utils;
 
 import com.store.data.generator.models.Employee;
 import com.store.data.generator.models.Item;
-import com.store.data.generator.utils.ItemSelector;
 import org.joda.time.DateTime;
+import java.util.ArrayList;
+import java.util.Random;
 
 import java.util.List;
 
 public class EmployeeSelector
 {
     public Employee generateEmp(DateTime date, Item item){
+        ArrayList<Employee> empList = new ArrayList<Employee>();
         for (Employee employee : employeeList){
             if(employee.getDepartment() == item.getDepartment())
             {
-                // random generate shit
+                empList.add(employee);
             }
         }
+        int randomNum = rand.nextInt(empList.size());
+
+        return empList.get(randomNum);
     }
 
-    public EmployeeSelector(final List<Employee> employeeList)
+    public EmployeeSelector(final List<Employee> employeeList, Random rand)
     {
         this.employeeList = employeeList;
+        this.rand = rand;
     }
 
     private List<Employee> employeeList;
+    private Random rand;
 }
