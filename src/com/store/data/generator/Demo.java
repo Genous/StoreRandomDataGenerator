abdd9cad110dfd921771126004eb76a416e0732e
knarjp <kevajess@gmail.com>
2016-11-01 18:17:28 -0400

    test employeeselector no rando

--- src/com/store/data/generator/Demo.java
+++ src/com/store/data/generator/Demo.java
@@ -65,6 +65,5 @@ public class Demo
 
         System.out.println(itemList.size());
     }
-}
 
 }
--- src/com/store/data/generator/utils/EmployeeSelector.java
+++ src/com/store/data/generator/utils/EmployeeSelector.java
@@ -1,5 +1,27 @@
 package com.store.data.generator.utils;
 
+import com.store.data.generator.models.Employee;
+import com.store.data.generator.models.Item;
+import com.store.data.generator.utils.ItemSelector;
+import org.joda.time.DateTime;
+
+import java.util.List;
+
 public class EmployeeSelector
 {
+    public Employee generateEmp(DateTime date, Item item){
+        for (Employee employee : employeeList){
+            if(employee.getDepartment() == item.getDepartment())
+            {
+                // random generate shit
+            }
+        }
+    }
+
+    public EmployeeSelector(final List<Employee> employeeList)
+    {
+        this.employeeList = employeeList;
+    }
+
+    private List<Employee> employeeList;
 }
