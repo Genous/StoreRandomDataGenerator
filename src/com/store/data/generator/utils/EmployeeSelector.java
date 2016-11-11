83cc6ab7547092e0f3efe6c0f1052125ec0cdf59
genous <cesar.atteh@gmail.com>
2016-11-11 15:54:22 -0500

    Optimized EmployeeSelect and ItemSelector

--- src/com/store/data/generator/utils/EmployeeSelector.java
+++ src/com/store/data/generator/utils/EmployeeSelector.java
@@ -1,33 +1,37 @@
 package com.store.data.generator.utils;
 
+import com.store.data.generator.models.Department;
 import com.store.data.generator.models.Employee;
 import com.store.data.generator.models.Item;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 import java.util.List;
 
 public class EmployeeSelector
 {
-    public Employee getEmp(Item item){
-        ArrayList<Employee> empList = new ArrayList<Employee>();
-        for (Employee employee : employeeList){
-            if(employee.getDepartment() == item.getDepartment())
-            {
-                empList.add(employee);
-            }
-        }
-        int randomNum = rand.nextInt(empList.size());
+    public Employee getEmp(Item item)
+    {
+        int randomNum = rand.nextInt(employeeMap.get(item.getDepartment()).size());
 
-        return empList.get(randomNum);
+        return employeeMap.get(item.getDepartment()).get(randomNum);
     }
 
     public EmployeeSelector(final List<Employee> employeeList, Random rand)
     {
-        this.employeeList = employeeList;
+        for(Department department : Department.values())
+        {
+            employeeMap.put(department, new ArrayList<>());
+        }
+        for (Employee employee : employeeList)
+        {
+            employeeMap.get(employee.getDepartment()).add(employee);
+        }
+
         this.rand = rand;
     }
 
-    private List<Employee> employeeList;
+    private HashMap<Department, ArrayList<Employee>> employeeMap = new HashMap<>();
     private Random rand;
 }
--- src/com/store/data/generator/utils/ItemSelector.java
+++ src/com/store/data/generator/utils/ItemSelector.java
@@ -1,6 +1,7 @@
 package com.store.data.generator.utils;
 
 import com.store.data.generator.models.Item;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.List;
@@ -9,23 +10,21 @@ public class ItemSelector
 {
     public Item getItem()
     {
-        ArrayList<Item> itList = new ArrayList<>();
+        //todo hash items based on type, then use the date to make patterns based on type. (t-shirts sell more in the summer etc...)
+        int randomNum = rand.nextInt(itemsArray.size());
 
-        for(Item items : itemList)
-        {
-            itList.add(items);
-        }
-        int randomNum = rand.nextInt(itList.size());
-
-        return itList.get(randomNum);
+        return itemsArray.get(randomNum);
     }
 
     public ItemSelector(final List<Item> itemList, Random rand)
     {
-        this.itemList = itemList;
+        for(Item items : itemList)
+        {
+            itemsArray.add(items);
+        }
         this.rand = rand;
     }
 
-    private List<Item> itemList;
+    private ArrayList<Item> itemsArray = new ArrayList<>();
     private Random rand;
 }
